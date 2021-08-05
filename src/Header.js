cd705792b93d8c22bdd092d121bb03d3d47bd9fc
Da Chu <dalice157@gmail.com>
2021-08-05 18:09:57 +0800

    deploy

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.scss';
 const Header= ({logo}) => {
   return (
     <div className={styles.header}>
-      <h1 className="logo">{logo}</h1>
+      <h1 className={styles.logo}>{logo}</h1>
     </div>
   )
 }
