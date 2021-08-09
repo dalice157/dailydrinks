8d11eded7120a65d5d285999fa59d7b0358a12ed
Da Chu <dalice157@gmail.com>
2021-08-09 10:27:27 +0800

    deploy

---
+++ precache-manifest.f516b101b50115d9ac1a92a33761d6b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475f88e9dbf0c0d830687998f8982c9e",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "a573452cd387a2955471",
+    "url": "/dailydrinks/static/css/main.410bab96.chunk.css"
+  },
+  {
+    "revision": "b1beec4beffe740f0e6e",
+    "url": "/dailydrinks/static/js/2.2410ca9c.chunk.js"
+  },
+  {
+    "revision": "a573452cd387a2955471",
+    "url": "/dailydrinks/static/js/main.a57260a6.chunk.js"
+  },
+  {
+    "revision": "67e660dad7067edadc76",
+    "url": "/dailydrinks/static/js/runtime~main.01af7d37.js"
+  }
+]);
